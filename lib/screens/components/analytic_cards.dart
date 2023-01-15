b67725840322cd29e2797e6938c61708532dd264
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-01-15 12:25:56 +0600

    Updated analytic_cards :+1:
    Inside Container  in
    child : Responsive(),
    :arrow_down:
    mobile, desktop, tablet Screen size are installed with

--- lib/screens/components/analytic_cards.dart
+++ lib/screens/components/analytic_cards.dart
@@ -1,4 +1,5 @@
 import 'package:admin_pannel/constants/constants.dart';
+import 'package:admin_pannel/constants/responsive.dart';
 import 'package:admin_pannel/data/data.dart';
 import 'package:flutter/material.dart';
 
@@ -9,14 +10,30 @@ class AnalyticCards extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Container(
-      child: AnalyticInfoCardGridView(),
+      child: Responsive(
+        mobile: AnalyticInfoCardGridView(
+          crossAxisCount: size.width < 650 ? 2 : 4,
+          childAspectRatio: size.width < 650 ? 2 : 1.5,
+        ),
+        desktop: AnalyticInfoCardGridView(
+          childAspectRatio: size.width < 1400 ? 1.5 : 2.1,
+        ),
+        tablet: AnalyticInfoCardGridView(),
+      ),
     );
   }
 }
 
 class AnalyticInfoCardGridView extends StatelessWidget {
-  const AnalyticInfoCardGridView({Key? key}) : super(key: key);
+  const AnalyticInfoCardGridView(
+      {Key? key, this.crossAxisCount = 4, this.childAspectRatio = 1.4})
+      : super(key: key);
+
+  final int crossAxisCount;
+  final double childAspectRatio;
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +42,10 @@ class AnalyticInfoCardGridView extends StatelessWidget {
       shrinkWrap: true,
       itemCount: analyticData.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4,
+        crossAxisCount: crossAxisCount,
         crossAxisSpacing: appPadding,
         mainAxisSpacing: appPadding,
-        childAspectRatio: 1.4,
+        childAspectRatio: childAspectRatio,
       ),
       itemBuilder: (context, index) => AnalyticInfoCard(
         info: analyticData[index],
