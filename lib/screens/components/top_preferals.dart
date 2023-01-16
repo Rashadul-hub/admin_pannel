212e421bd8745a4dc3da3796006d6d46ac20a1b0
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-01-17 02:05:02 +0600

    Create & Modified The Class With  Column & Text

---
+++ lib/screens/components/top_preferals.dart
@@ -0,0 +1,41 @@
+import 'package:admin_pannel/constants/constants.dart';
+import 'package:admin_pannel/data/data.dart';
+import 'package:admin_pannel/screens/components/referal_info_detail.dart';
+import 'package:flutter/material.dart';
+
+class TopPreferals extends StatelessWidget {
+  const TopPreferals({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 350,
+      padding: EdgeInsets.all(appPadding),
+      decoration: BoxDecoration(
+        color: secondaryColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Top Referals',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w700,
+                  color: textColor,
+                ),
+              ),
+              Text(
+                'View All',
+                style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                  color: textColor.withOpacity(0.5),
+                ),
+              ),
+            ],
+          ),
