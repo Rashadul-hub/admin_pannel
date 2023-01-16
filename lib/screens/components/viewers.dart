e86c7646c81a6e2396c6e1c2f6042de161dc0696
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-01-17 02:08:54 +0600

    Created Viewers
    Showing all the Viewers

---
+++ lib/screens/components/viewers.dart
@@ -0,0 +1,35 @@
+import 'package:admin_pannel/constants/constants.dart';
+import 'package:admin_pannel/screens/components/view_line_chart.dart';
+import 'package:flutter/material.dart';
+
+class Viewers extends StatelessWidget {
+  const Viewers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 350,
+      padding: EdgeInsets.all(appPadding),
+      decoration: BoxDecoration(
+        color: secondaryColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Viewers",
+            style: TextStyle(
+              color: textColor,
+              fontSize: 15,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Expanded(
+            child: ViewLineChart(),
+          )
+        ],
+      ),
+    );
+  }
+}
