568f431dd647cd709be020cea4995bd6f5aee7f6
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-01-17 21:41:53 +0600

    Modified Container

--- lib/screens/components/users.dart
+++ lib/screens/components/users.dart
@@ -8,23 +8,6 @@ class Users extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      width: double.infinity,
-      padding: EdgeInsets.all(appPadding),
-      decoration: BoxDecoration(
-        color: secondaryColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Users",
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 15,
-              color: textColor,
             ),
           ),
           Expanded(child: BarChartUsers())
