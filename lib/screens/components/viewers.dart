35b05d2b17cdb00c6997fb24d0add0a3597f6822
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-01-17 21:45:36 +0600

    Updated in Container
    For Viewers

--- lib/screens/components/viewers.dart
+++ lib/screens/components/viewers.dart
@@ -7,6 +7,25 @@ class Viewers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(appPadding),
+      child: Container(
+        height: 350,
+        padding: EdgeInsets.all(appPadding),
+        decoration: BoxDecoration(
+          color: secondaryColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Viewers",
+              style: TextStyle(
+                color: textColor,
+                fontSize: 15,
+                fontWeight: FontWeight.w700,
+              ),
             ),
           ),
           Expanded(
