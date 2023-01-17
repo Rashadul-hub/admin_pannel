da859c257ed813ea340b42ebbfcebeede17cb860
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-01-17 21:42:51 +0600

    Modified Container

--- lib/screens/components/users_by_device.dart
+++ lib/screens/components/users_by_device.dart
@@ -7,34 +7,6 @@ class UsersByDevice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 350,
-      decoration: BoxDecoration(
-        color: secondaryColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      padding: EdgeInsets.all(appPadding),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Users by devices",
-            style: TextStyle(
-              color: textColor,
-              fontSize: 15,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(appPadding),
-            padding: EdgeInsets.all(appPadding),
-            height: 230,
-            child: CustomPaint(
-              foregroundPainter: RadialPainter(
-                bgColor: textColor.withOpacity(0.1),
-                lineColor: primaryColor,
-                percent: 0.7,
-                width: 18.0,
               ),
               child: Center(
                 child: Text(
