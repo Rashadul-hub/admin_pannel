354abd2872455b4aa5d501f76dbaa43c8442b15c
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-01-17 21:45:02 +0600

    Updated Line Chart

--- lib/screens/components/view_line_chart.dart
+++ lib/screens/components/view_line_chart.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:admin_pannel/constants/constants.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
@@ -32,50 +34,51 @@ class _ViewLineChartState extends State<ViewLineChart> {
           titlesData: FlTitlesData(
             show: true,
             bottomTitles: SideTitles(
-                showTitles: true,
-                reservedSize: 22,
-                getTextStyles: defaultGetTitleTextStyle,
-                getTitles: (value) {
-                  switch (value.toInt()) {
-                    case 1:
-                      return 'Sun';
-                    case 4:
-                      return 'Mon';
-                    case 7:
-                      return 'Tue';
-                    case 10:
-                      return 'Wed';
-                    case 13:
-                      return 'Thr';
-                    case 16:
-                      return 'Fri';
-                    case 19:
-                      return 'Sat';
-                  }
-                  return '';
-                }),
+              showTitles: false,
+              reservedSize: 22,
+              getTextStyles: defaultGetTitleTextStyle,
+              getTitles: (value) {
+                switch (value.toInt()) {
+                  case 1:
+                    return 'Sun';
+                  case 4:
+                    return 'Mon';
+                  case 7:
+                    return 'Tue';
+                  case 10:
+                    return 'Wed';
+                  case 13:
+                    return 'Thr';
+                  case 16:
+                    return 'Fri';
+                  case 19:
+                    return 'Sat';
+                }
+                return '';
+              },
+            ),
           ),
           borderData: FlBorderData(
             show: false,
           ),
-          minX: 0,
+          minX: 3,
           maxX: 20,
-          maxY: 0,
+          maxY: 1,
           minY: 6,
           lineBarsData: [
             LineChartBarData(
               spots: [
                 FlSpot(1, 3),
-                FlSpot(10, 1),
-                FlSpot(9, 4),
-                FlSpot(12, 3),
-                FlSpot(10, 5),
+                FlSpot(4, 1),
+                FlSpot(7, 4),
+                FlSpot(10, 3),
+                FlSpot(13, 5),
                 FlSpot(16, 3),
-                FlSpot(20, 4),
+                FlSpot(19, 4),
               ],
               isCurved: true,
               colors: [primaryColor],
-              barWidth: 5,
+              barWidth: 2,
               isStrokeCapRound: true,
               dotData: FlDotData(show: true),
               belowBarData: BarAreaData(
